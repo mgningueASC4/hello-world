103eda09ee9d351e4b46ac6c7894662e2695b3c6
Mohammadou Gningue <gninguem@bxscience.edu>
2018-11-10 19:33:54 +0000

    ㅛㅐㅎ

--- ajax.js
+++ ajax.js
@@ -1,11 +1,14 @@
 var slideIndex = 0;
 showSlides();
+
+
 function showSlides(){
-    var slides = document.getElementsByClassName("myimages")
+    //console.log('hello', slideIndex);
+    var slides = document.getElementsByClassName("myimages");
     for (var i = 0; i<slides.length; i++){
         slides[i].style.display="none";
     }
-    slideIndex++
+    slideIndex++;
     if (slideIndex >= slides.length){
         slideIndex = 0;
     }
@@ -13,3 +16,4 @@ function showSlides(){
     setTimeout(showSlides, 4000);
     
 }
+
